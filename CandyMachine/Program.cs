f2caa515eacc69669d42c94cf41fab7d08956e3d
Gillesjr12 <gillesbrouillette66@gmail.com>
2022-03-10 22:27:24 -0500

    Creation Main / debut projet

---
+++ CandyMachine/Program.cs
@@ -0,0 +1,26 @@
+﻿using Project;
+using static Project.Board;
+using static Project.Candy;
+using static Project.Data;
+using System.Globalization;
+using System.Threading;
+
+
+namespace CandyMachine
+{
+    
+    class Program
+    {
+        
+        static void Main()
+        {
+
+            int output = 0;
+            bool tryparse = false;
+            Data dataCandy = new Data();
+            Candy[] candies = dataCandy.LoadCandies();
+            Console.WriteLine($"{candies[1].Name}");
+
+        }
+    }
+}
