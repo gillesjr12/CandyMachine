acdb5296cb3fa7bedca653e026692c60eabcd4f9
Gillesjr12 <gillesbrouillette66@gmail.com>
2022-03-10 23:13:52 -0500

    Creation function GetSelection()

--- CandyMachine/Program.cs
+++ CandyMachine/Program.cs
@@ -8,19 +8,45 @@ using System.Threading;
 
 namespace CandyMachine
 {
-    
+
     class Program
     {
-        
+
+        static byte GetSelection(byte input)
+        {
+            input = 0;
+            string message = "Votre choix";
+            bool tryParse = false;
+            Print(PadBoth(message,input));
+            do
+            {
+                Console.Write("Veuillez faire votre selection [1-25] : ");
+                tryParse = byte.TryParse(Console.ReadLine(), out input);
+                if (input<0 || input>25)
+                {
+                    Console.WriteLine("Veuillez entrez un nombre valide.");
+                }
+                else if (input>0 || input <25)
+                {
+                    Print(PadBoth(message,input));
+                }
+            } while (tryParse != true);
+
+            return input;
+        }
+
         static void Main()
         {
 
-            int output = 0;
+            byte output = 0;
             bool tryparse = false;
             Data dataCandy = new Data();
             Candy[] candies = dataCandy.LoadCandies();
             Console.WriteLine($"{candies[1].Name}");
+            GetSelection(output);
 
         }
+        
     }
+
 }
